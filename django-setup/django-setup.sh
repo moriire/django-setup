eda66f9ebe729b9c2e89d71dc0fce1c7ee892948
Mobolaji Abdulsalam <ibraheemabdulsalam@gmail.com>
2021-12-09 18:42:18 +0100

    Create django-setup.sh

---
+++ django-setup/django-setup.sh
@@ -0,0 +1,135 @@
+source config.sh;
+cat 007.txt;
+
+errors(){
+        red='\033[31m';
+        nc='\033[0m';
+        txt="$red $1 $nc";
+        echo -e $txt;
+};
+
+success(){                               green='\033[32m';               nc='\033[0m';                   txt="$green $1 $nc";              echo -e $txt;
+};
+
+pers(){
+        while read -p $1 $2;
+do
+        if [ ! $2 ]; then
+                echo $1;
+                continue
+        return $2;
+        fi;
+        done;
+        };
+
+venv(){                             python3 -m venv env;        source env/bin/activate;                                pip install django;};
+proj() {
+        #source env/bin/activate;
+        if [ -d $1 ];
+        then
+                errors "$1 already exists";
+                cd $1;
+        else
+                django-admin startproject $1 && success "project $1 created";
+cd $1;
+        fi;
+};
+
+
+
+app(){
+        if [ -d './$2/$1' ]; then
+                errors $1 already exists.
+        else
+                python3 manage.py startapp $1 && success "an app $1 is created on project $2";
+        fi;
+};
+
+config(){
+        cd $1;
+        success "Development starting at $1";
+};
+dpurls(){
+        if [ -f "./$1/urls.py" ];
+        then
+        rm "./$1/urls.py" &&            touch "./$1/urls.py";
+else
+        touch "./$1/urls.py";
+        fi;
+echo -e purl >> "./$1/urls.py";
+};
+
+dpapps(){
+        if [ -f "./$2/urls.py" ];
+                then
+                        rm "./$2/urls.py";            touch "./$2/urls.py";
+                else
+                        touch "./$2/urls.py";
+                        fi;
+                        echo -e aurl >> "./$2/urls.py";                        };
+
+appview(){
+        if [ -f "./$2/views.py" ]; then
+        rm "./$2/views.py";
+        touch "./$2/views.py";
+else
+        touch "./$2/views.py";
+        fi;
+echo -e appv >> "./$2/views.py";
+};
+
+psettings(){                               rm "./$1/settings.py";            touch "./$1/settings.py";         echo -e settings >> "./$1/settings.py";                     };
+
+while read -p "Dev folder: " devloc;
+do
+        if [[ $devloc == '' || $devloc == ' ' ]]; then
+                errors "Input cannot be empty. Give your root folder a name";
+                continue;
+        else
+                config $devloc;
+                break
+        fi
+done;
+
+if [ ! -d django-setup ];
+then
+        echo starting;
+mkdir django-setup && cd django-setup;
+echo setting up environment..
+venv;
+echo Virtual environment created.;
+else
+        cd django-setup;
+        errors "Like this project already exists";
+        echo continue...;
+fi;
+
+read -p  "Project Name[unamed]: " projname;
+if [[ $projname == '' || $projname == ' ' ]]; then
+        projname='unamed';
+fi;
+
+        if [ ! -d $projname ]; then
+                proj $projname;
+else
+        rm -r $projname;
+        proj $projname;
+fi;
+        read -p "App Name: " appname;
+        app $appname $projname &&
+        dpurls $projname $appname && echo project urls configured && dpapps $projname $appname && echo $appname urls configured sucessfully. &&
+        psettings $projname;
+        appview $projname $appname && echo home page successfully setup.;
+        if [ ! -d "./$appname/templates/" ];
+        then
+                mkdir "./$appname/templates"
+                else
+                        echo templates folder created;
+        fi;
+
+        if [ ! -d "./$appname/static/" ];
+        then
+                mkdir "./$appname/static"
+                else
+                        echo static folder created... ;
+        fi;
